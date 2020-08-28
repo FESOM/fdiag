be6e6a8f67ff6aeea47eb4655f9be053fe104056
Nikolay Koldunov <koldunovn@gmail.com>
2020-08-28 11:56:25 +0200

    refactoring

--- fcompare.py
+++ fcompare.py
@@ -20,29 +20,33 @@ templates_path = pkg_resources.resource_filename(__name__, f"templates_html")
 file_loader = FileSystemLoader(templates_path)
 env = Environment(loader=file_loader)
 
-workflow_name = "test"
-
-workflow_settings = "compare_two_CORE_ollie.yml"
-
-
-input_paths = [
-    "/Users/nkolduno/PYTHON/DATA/output_7_8/",
-    "/Users/nkolduno/PYTHON/DATA/output_7_10",
-]
-
-input_names = None
-
-if input_names is None:
-    input_names = []
-    for run in input_paths:
-        run = os.path.join(run, "")
-        input_names.append(run.split("/")[-2])
-elif len(input_names) != len(input_paths):
-    raise ValueError("The size of input_names is not equal to the size of input_paths")
-
+workflow_settings = "short.yml"
 
 with open(workflow_settings) as file:
     settings = yaml.load(file, Loader=yaml.FullLoader)
+
+workflow_name = settings['workflow_name']
+
+input_paths = settings['input_paths']
+if "input_names" in settings:
+    input_names = settings['input_names']
+else:
+    input_names = None
+
+
+def check_input_names(input_names, input_paths):
+    if input_names is None:
+        input_names = []
+        for run in input_paths:
+            run = os.path.join(run, "")
+            input_names.append(run.split("/")[-2])
+    elif len(input_names) != len(input_paths):
+        raise ValueError("The size of input_names is not equal to the size of input_paths")
+    
+    return input_names
+
+input_names = check_input_names(input_names, input_paths)
+print(input_names)
 
 ofolder = f"./results/{workflow_name}"
 
@@ -63,45 +67,22 @@ webpages["general"] = {}
 webpages["general"]["name"] = settings["workflow_name"]
 webpages["analyses"] = {}
 
-if 'difference' in settings:
-    webpage = drive_difference(settings, 'difference')
-    webpages['analyses']['difference'] = webpage
+analyses = {}
+analyses['difference'] = drive_difference
+analyses['climatology'] = drive_difference
+analyses['ice_integrals'] = drive_ice_integrals
+analyses['hovm_difference'] = drive_hovm_difference
+analyses['hovm_difference_clim'] = drive_hovm_difference
+analyses['ocean_integrals'] = drive_ocean_integrals
+analyses['xmoc'] = drive_xmoc
+analyses['xmoc_difference'] = drive_xmoc
+analyses['amoc_timeseries'] = drive_amoc_timeseries
+analyses['vertical_profile'] = drive_vertical_profile
 
-if 'climatology' in settings:
-    webpage = drive_difference(settings, 'climatology')
-    webpages['analyses']['climatology'] = webpage
-
-if 'ice_integrals' in settings:
-    webpage = drive_ice_integrals(settings, 'ice_integrals')
-    webpages['analyses']['ice_integrals'] = webpage
-
-if 'hovm_difference' in settings:
-    webpage = drive_hovm_difference (settings, 'hovm_difference')
-    webpages['analyses']['hovm_difference'] = webpage
-
-if 'hovm_difference_clim' in settings:
-    webpage = drive_hovm_difference (settings, 'hovm_difference_clim')
-    webpages['analyses']['hovm_difference_clim'] = webpage
-
-if "ocean_integrals" in settings:
-    webpage = drive_ocean_integrals(settings, "ocean_integrals")
-    webpages["analyses"]["ocean_integrals"] = webpage
-
-if "xmoc" in settings:
-    webpage = drive_xmoc(settings, "xmoc")
-    webpages["analyses"]["xmoc"] = webpage
-
-if "xmoc_difference" in settings:
-    webpage = drive_xmoc(settings, "xmoc_difference")
-    webpages["analyses"]["xmoc_difference"] = webpage
-
-if "amoc_timeseries" in settings:
-    webpage = drive_amoc_timeseries(settings, "amoc_timeseries")
-    webpages["analyses"]["amoc_timeseries"] = webpage
-
-if "vertical_profile" in settings:
-    webpage = drive_vertical_profile(settings, "vertical_profile")
-    webpages["analyses"]["vertical_profile"] = webpage
+for analysis in analyses:
+    if analysis in analyses:
+        webpage = analyses[analysis](settings, analysis)
+        webpages['analyses'][analysis] = webpage
 
 
 # ofolder = f"{experiment_path}/"
