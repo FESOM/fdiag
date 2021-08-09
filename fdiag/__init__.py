93ccf99d27dc6dd9d51e096c72ed6b6f034fb422
Nikolay Koldunov <koldunovn@gmai.com>
2021-08-09 15:15:09 +0000

    fix setup

---
+++ fdiag/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# fdiag - set of diagnostics for FESOM2
+#
+
+"""Top-level package for fdiag."""
+
+__author__ = """FESOM team"""
+__email__ = "koldunovn@gmail.com"
+__version__ = "0.1.0"
+
+from .fdiag import *
+
